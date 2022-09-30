baace84bc8a09358bbe64b89534501da86810921
Arkadiy Shneider <shnr.arky@gmail.com>
2022-09-30 22:25:48 +1000

    Добавил в репо файл настроек
    Пока репо приватный настройки будут лежать здесь, потом надо убрать

---
+++ src/settings.py
@@ -0,0 +1,13 @@
+api_token = '9596.uOf2NzYzM1nexg7Rq'
+bot_token = '5258425084:AAEAjkT-W58KLSLC9vfj4fO9k4g5PT-IphE'
+
+tags = {
+    'Фрукты': {
+        'Яблоки': [
+            'Яблоки'
+        ],
+        'Бананы': [
+            'Бананы'
+        ]
+    }
+}
