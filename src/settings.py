8f1dd2abe919ad4777b29fdbc2599af3497e9c7e
Arkadiy Shneider <shnr.arky@gmail.com>
2022-10-06 14:14:04 +1000

    Обновил файл настроек
    По глупости скинул заполненный файл. Все токены и пароли поменял, сохраненные в прошлом коммите более не актуальные

--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,1 @@
-api_token = '9596.uOf2NzYzM1nexg7Rq'
-bot_token = '5258425084:AAEAjkT-W58KLSLC9vfj4fO9k4g5PT-IphE'
-
-host = '95.154.68.63'
-port = 3306
-user = 'student'
-password = 'Hn523cv-'
-db = 'receipts'
-charset = 'utf8mb4'
+''' Здесь должны быть указаны данные для подключения к БД, токен доступа для бота и токен доступ к api получения чека из ФНС '''
